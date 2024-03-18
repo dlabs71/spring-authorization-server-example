ce5a25f4beb86243a2c5c8814eabc6af9965a29a
dlabs71 <danila.a.ivanov@d-labs.ru>
2024-03-18 13:57:55 +0300

    Corrected the authentication entry point in the Authorization Server Config

--- j-sso/src/main/java/ru/dlabs/sas/example/jsso/config/security/AuthorizationServerConfig.java
+++ j-sso/src/main/java/ru/dlabs/sas/example/jsso/config/security/AuthorizationServerConfig.java
@@ -1,7 +1,11 @@
 package ru.dlabs.sas.example.jsso.config.security;
 
+import static ru.dlabs.sas.example.jsso.config.security.SecurityConfig.LOGIN_PAGE;
+import static ru.dlabs.sas.example.jsso.config.security.SecurityConfig.PERMIT_ALL_PATTERNS;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,10 +29,6 @@ import ru.dlabs.sas.example.jsso.dto.AuthorizedUser;
 import ru.dlabs.sas.example.jsso.dto.IntrospectionPrincipal;
 import ru.dlabs.sas.example.jsso.dto.TokenInfoDto;
 
-import java.io.IOException;
-
-import static ru.dlabs.sas.example.jsso.config.security.SecurityConfig.PERMIT_ALL_PATTERNS;
-
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
@@ -49,59 +49,81 @@ public class AuthorizationServerConfig {
 
         RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
         http.securityMatcher(endpointsMatcher)
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                // ендпоинты которые вынесем из под security
-                                .requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
-                                .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
-                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
-                .apply(authorizationServerConfigurer);
+            .authorizeHttpRequests(authorize ->
+                                       authorize
+                                           // ендпоинты которые вынесем из под security
+                                           .requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
+                                           .anyRequest().authenticated()
+            )
+            .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
+
+            // После смены страницы логина, не забываем и тут изменить
+            .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(
+                new LoginUrlAuthenticationEntryPoint(LOGIN_PAGE)
+            ))
+            .apply(authorizationServerConfigurer);
         return http.build();
     }
 
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder()
-                .issuer(authorizationServerProperties.getIssuerUrl())
-                .tokenIntrospectionEndpoint(authorizationServerProperties.getIntrospectionEndpoint())
-                .build();
+            .issuer(authorizationServerProperties.getIssuerUrl())
+            .tokenIntrospectionEndpoint(authorizationServerProperties.getIntrospectionEndpoint())
+            .build();
     }
 
-    private void introspectionResponse(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        OAuth2TokenIntrospectionAuthenticationToken introspectionAuthenticationToken = (OAuth2TokenIntrospectionAuthenticationToken) authentication;
-        TokenInfoDto.TokenInfoDtoBuilder tokenInfoDtoBuilder = TokenInfoDto.builder().active(false);                        // создаём билдер объекта ответа
-        if (introspectionAuthenticationToken.getTokenClaims().isActive()) {                                                 // если токен активен, то заполняем все параметры информации о токене и далее пытаемся получить информацию о пользователе
+    private void introspectionResponse(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication
+    ) throws IOException {
+        OAuth2TokenIntrospectionAuthenticationToken introspectionAuthenticationToken =
+            (OAuth2TokenIntrospectionAuthenticationToken) authentication;
+        TokenInfoDto.TokenInfoDtoBuilder tokenInfoDtoBuilder =
+            TokenInfoDto.builder().active(false);                        // создаём билдер объекта ответа
+        if (introspectionAuthenticationToken.getTokenClaims()
+            .isActive()) {                                                 // если токен активен, то заполняем все параметры информации о токене и далее пытаемся получить информацию о пользователе
             OAuth2TokenIntrospection claims = introspectionAuthenticationToken.getTokenClaims();
             tokenInfoDtoBuilder.active(true)
-                    .sub(claims.getSubject())
-                    .aud(claims.getAudience())
-                    .nbf(claims.getNotBefore())
-                    .scopes(claims.getScopes())
-                    .iss(claims.getIssuer())
-                    .exp(claims.getExpiresAt())
-                    .iat(claims.getIssuedAt())
-                    .jti(claims.getId())
-                    .clientId(claims.getClientId())
-                    .tokenType(claims.getTokenType());
+                .sub(claims.getSubject())
+                .aud(claims.getAudience())
+                .nbf(claims.getNotBefore())
+                .scopes(claims.getScopes())
+                .iss(claims.getIssuer())
+                .exp(claims.getExpiresAt())
+                .iat(claims.getIssuedAt())
+                .jti(claims.getId())
+                .clientId(claims.getClientId())
+                .tokenType(claims.getTokenType());
 
 
-            String token = introspectionAuthenticationToken.getToken();                                                      // получаем значение токена, который проверяется
-            OAuth2Authorization tokenAuth = oAuth2AuthorizationService.findByToken(token, OAuth2TokenType.ACCESS_TOKEN);     // предполагая что это ACCESS TOKEN, пытаемся получить объект OAuth2Authorization из OAuth2AuthorizationService
+            String token =
+                introspectionAuthenticationToken.getToken();                                                      // получаем значение токена, который проверяется
+            OAuth2Authorization tokenAuth = oAuth2AuthorizationService.findByToken(
+                token,
+                OAuth2TokenType.ACCESS_TOKEN
+            );     // предполагая что это ACCESS TOKEN, пытаемся получить объект OAuth2Authorization из OAuth2AuthorizationService
             if (tokenAuth != null) {
-                Authentication attributeAuth = tokenAuth.getAttribute(principalAttributeKey);                                // Если найден этот объект OAuth2Authorization, то получаем из него объект Authentication следующим образом
+                Authentication attributeAuth =
+                    tokenAuth.getAttribute(principalAttributeKey);                                // Если найден этот объект OAuth2Authorization, то получаем из него объект Authentication следующим образом
                 if (attributeAuth != null) {
                     if (attributeAuth.getPrincipal() instanceof AuthorizedUser authorizedUser) {                             // Если полученный объект Authentication не пуст, то проверяем является ли его principal экземпляром класса AuthorizedUser
                         tokenInfoDtoBuilder.principal(IntrospectionPrincipal.build(authorizedUser));                         // Создаём IntrospectionPrincipal на его основе
                     } else {                                                                                                 // Иначе выбрасываем исключение, что другие типы principal мы не поддерживаем
-                        throw new RuntimeException("Principal class = " + attributeAuth.getPrincipal().getClass().getSimpleName() + " is not supported");
+                        throw new RuntimeException(
+                            "Principal class = " + attributeAuth.getPrincipal().getClass().getSimpleName() +
+                                " is not supported");
                     }
                 }
             }
         }
 
         ServletServerHttpResponse httpResponse = new ServletServerHttpResponse(response);
-        mappingJackson2HttpMessageConverter.write(tokenInfoDtoBuilder.build(), null, httpResponse);              // Превращаем наш TokenInfoDto в json строку и отправляем её через ServletServerHttpResponse
+        mappingJackson2HttpMessageConverter.write(
+            tokenInfoDtoBuilder.build(),
+            null,
+            httpResponse
+        );              // Превращаем наш TokenInfoDto в json строку и отправляем её через ServletServerHttpResponse
     }
 }
